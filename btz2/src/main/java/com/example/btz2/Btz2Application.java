f9522dca6a7c6cb4f76c82d9abfce0ac1ac64daa
johordan david mendoza florez <johordanmendozaf@gmail.com>
2026-08-28 08:17:28 -0500

    ok

---
+++ btz2/src/main/java/com/example/btz2/Btz2Application.java
@@ -0,0 +1,13 @@
+package com.example.btz2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Btz2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Btz2Application.class, args);
+	}
+
+}
